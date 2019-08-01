ced7a2b89b0fd3ee6d05e1b324596e1dc4fd022d
Rohit Hill <torohithill@gmail.com>
2019-08-01 19:51:35 +0530

    Bubble Sort in python

---
+++ Sorting/Bubble Sort/bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(a):
+    ''' Sorts the given array in ascending order '''
+    for i in range(len(a)):
+        for j in range(i+1,len(a)):
+            if a[i] > a[j]:
+                a[i],a[j] = a[j],a[i]
+
+if __name__ == '__main__':
+    a = [3,1,2,-1,0]
+    bubble_sort(a)
+    assert a == [-1,0,1,2,3]
+
+
